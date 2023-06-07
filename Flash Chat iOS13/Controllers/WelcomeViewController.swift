1b159679833322c777a462e6c8ea82dab4d8ab9e
ducnguyenp <duc.np@iparamed.com>
2023-06-07 22:40:13 +0700

    git push

---
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WelcomeViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        logInButton.layer.cornerRadius = 10
+        registerButton.layer.cornerRadius = 10
+    }
+    
+
+}
